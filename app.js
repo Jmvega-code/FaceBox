ed08e80403a9dcb032ddac58e3df4d59521b7419
Juanma Vega <juanmavega83@gmail.com>
2020-02-03 16:55:01 +0100

    Data Persistence, adding mongoose

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const express    = require('express'),
+      app        = express(),
+      bodyParser = require('body-parser'),
+      mongoose   = require('mongoose')
+
+mongoose.connect('mongodb://localhost/boxes_app',{useNewUrlParser: true, useUnifiedTopology: true});
+
+const boxSchema = new mongoose.Schema({
+  name: String,
+  image: String
+}),
+Box = mongoose.model('Box', boxSchema)
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine', 'ejs');
-let boxes = [
+
+
+
+
+
+
+
+/**let boxes = [
   {name: 'Crossfit Montequinto', image: 'https://bit.ly/2S4JNdU'},
   {name: 'Giralda Crossfit', image: 'https://bit.ly/36HPjIN'},
   {name: 'La Colmena Crossfit', image: 'https://bit.ly/2RFcRcX'},
@@ -15,20 +31,33 @@ let boxes = [
   {name: 'Giralda Crossfit', image: 'https://bit.ly/36HPjIN'},
   {name: 'La Colmena Crossfit', image: 'https://bit.ly/2RFcRcX'}
 ];
-
+*/
 app.get('/', (req, res) => {
   res.render('landing');
 })
 
 app.get('/boxes', (req,res) => {
-  res.render('boxes', {boxes})
+  // Get all boxes from db
+  Box.find({}, (err, allBoxes) => {
+    if(err){
+      console.log(err)
+    } else {
+      res.render('boxes', {allBoxes})
+    }
+  })
 });
 
 app.post('/boxes', (req,res) => {
   let name = req.body.name;
   let image = req.body.image;
   let newbox = {name: name, image: image};
-  boxes.push(newbox);
+  Box.create(newbox, (err, newbox) => {
+    if(err){
+      console.log(err);
+    } else {
+      console.log(newbox);
+    }
+  })
   res.redirect('/boxes');
 })
 app.get('/boxes/new', (req,res) => {
