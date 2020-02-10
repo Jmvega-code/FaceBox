a0ddd0259df7939fe17922a052852d3ae48f84a0
Juanma Vega <juanmavega83@gmail.com>
2020-02-10 11:46:55 +0100

    express sanitizer and method override packages

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const express    = require('express'),
-      app        = express(),
-      bodyParser = require('body-parser'),
-      mongoose   = require('mongoose')
+const bodyParser        = require('body-parser'),
+      express           = require('express'),
+      methodOverride    = require('method-override'),
+      expressSanitizer  = require('express-sanitizer,')
+      app               = express(),
+      mongoose          = require('mongoose')
 
 mongoose.connect('mongodb://localhost/boxes_app',{useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -39,11 +41,7 @@ app.get('/boxes/new', (req,res) => {
 
 // CREATE add new Box to db
 app.post('/boxes', (req,res) => {
-  let name = req.body.name;
-  let image = req.body.image;
-  let address= req.body.address;
-  let newbox = {name, image, address};
-  Box.create(newbox, (err, newbox) => {
+  Box.create(req.body.box, (err, newBox) => {
     if(err){
       console.log(err);
     } else {
@@ -60,9 +58,41 @@ app.get('/boxes/:id', (req,res) => {
       console.log(err);
     } else {
       res.render('show', {foundBox})
-
     }
   })
+});
+
+// EDIT shows edit form and update a post
+app.get('/boxes/:id/edit', (req,res) => {
+  Box.findById(req.params.id, (err, foundBox) => {
+    if(err){
+      res.redirect('/boxes');
+    } else {
+      res.render('edit', {box: foundBox});
+    }
+  });
+});
+
+// UPDATE ROUTE
+app.put('/boxes/:id', (req, res) => {
+  Box.findByIdAndUpdate(req.params.id, req.body.box, (err, updatedBox) => {
+    if(err){
+      res.redirect('/boxes');
+    } else {
+      res.redirect('/post/' + req.params.id)
+    }
+  });
+});
+
+// DELETE ROUTE
+app.delete('/boxes/:id', (req, res) => {
+  Box.findByIdAndRemove(req.params.id, (err,) => {
+    if(err){
+      res.redirect('/boxes');
+    } else {
+      res.redirect('/boxes')
+    }
+  });
 });
 
 app.listen(3000, () => {
